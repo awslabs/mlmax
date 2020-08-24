0c12a1e822dd9cb842bc848f30d5fd73f4cceb8e
yihyap <yihyap@amazon.com>
2020-08-24 08:54:05 +0800

    delete template folder

--- template/src/lambda.py
+++
@@ -1,9 +0,0 @@
-import json
-import os
-
-# use by serverless lambda
-def lambda_handler(event, context):
-    print("Full event data:")
-    print(event)
-
-    return {"statusCode": 200, "body": json.dumps("Hello from Lambda!")}
