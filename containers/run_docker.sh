5123b9bd2d34fae8ef6353ea1183550ac14b85df
Josiah Davis <josiah.j.davis@gmail.com>
2020-10-20 14:05:46 +0800

    deleting dot files and docker files

--- containers/run_docker.sh
+++
@@ -1,9 +0,0 @@
-APP_DIR=$(PWD)
-APP_DIR=${APP_DIR%/*}
-
-docker stop basic
-docker rm basic
-docker rmi basicimage
-docker build --tag basicimage basic
-docker run -v $APP_DIR:/app -d --name basic basicimage
-docker exec -it basic /bin/bash
--- dotfiles/link_dot.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-ln -s $HOME/dotfiles/.vimrc $HOME/.vimrc
-ln -s $HOME/dotfiles/.tmux.conf $HOME/.tmux.conf
