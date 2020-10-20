c57a78ea0d5d485265905f0fccd36dc1b852e5d1
Josiah Davis <josiah.j.davis@gmail.com>
2020-10-20 13:58:58 +0800

    removing app/ files which can temporarily live in another branch

--- modules/app/ui/run_docker.sh
+++
@@ -1,9 +0,0 @@
-APP_DIR=$(PWD)
-#APP_DIR=${APP_DIR%/*}
-
-docker stop app
-docker rm app
-#docker rmi appimage
-#docker build --tag appimage .
-docker run -v $APP_DIR:/app -p 3000:3000 -d --name app appimage
-docker exec -it app /bin/bash
