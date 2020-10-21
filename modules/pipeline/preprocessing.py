f2320f60b9163c8407fd479c552dcf24ca8abf27
Josiah Davis <josiah.j.davis@gmail.com>
2020-10-21 14:40:00 +0800

    inital commit for working preprocesing in main guard

--- modules/pipeline/preprocessing.py
+++ modules/pipeline/preprocessing.py
@@ -2,6 +2,7 @@ import argparse
 import os
 import warnings
 
+from io import StringIO
 import numpy as np
 import pandas as pd
 import tarfile
@@ -22,7 +23,6 @@ try:
 except:
     import joblib
 
-from io import StringIO
 
 columns = [
     "age",
@@ -83,14 +83,6 @@ original_columns = [
 class_labels = [" - 50000.", " 50000+."]
 
 
-def print_shape(df):
-    negative_examples, positive_examples = np.bincount(df["income"])
-    print(
-        "Data shape: {}, {} positive examples, {} negative examples".format(
-            df.shape, positive_examples, negative_examples
-        )
-    )
-
 def model_fn(model_dir):
     """
     This loads returns a Scikit-learn Classifier from a model.joblib file in
@@ -100,14 +92,16 @@ def model_fn(model_dir):
     return clf
 
 
-# TODO try to comment out the "input_fn" function below, see what happens?
 def input_fn(request_body, request_content_type):
-    """Takes request data and de-serializes the data into an object for prediction.
-        When an InvokeEndpoint operation is made against an Endpoint running SageMaker model server,
-        the model server receives two pieces of information:
+    """
+        Takes request data and de-serializes the data into an object for
+        prediction.  When an InvokeEndpoint operation is made against an
+        Endpoint running SageMaker model server, the model server receives two
+        pieces of information:
             - The request Content-Type, for example "application/json"
             - The request data, which is at most 5 MB (5 * 1024 * 1024 bytes) in size.
-        The input_fn is responsible to take the request data and pre-process it before prediction.
+        The input_fn is responsible to take the request data and pre-process it
+        before prediction.
     Args:
         input_data (obj): the request data.
         content_type (str): the request Content-Type.
@@ -122,12 +116,14 @@ def input_fn(request_body, request_content_type):
     with open(input_data_path, "w") as fout:
         fout.write(request_body)
 
-    print("Reading input data from {}".format(input_data_path))
+    print(f"Reading input data from {input_data_path}")
     df = pd.read_csv(input_data_path)
     # df = pd.read_csv(input_data_path, names=original_columns)
     print("Just read it in", df.head(1))
     print("Before selection of DF = ", df.shape)
-    df = pd.DataFrame(data=df, columns=columns[:-1]) # get rid of the income column (label)
+    df = pd.DataFrame(
+        data=df, columns=columns[:-1]
+    )  # get rid of the income column (label)
     print("After selection of DF = ", df.shape)
     print(f"null sum = {df.isnull().sum()}")
     df.dropna(inplace=True)
@@ -140,8 +136,8 @@ def input_fn(request_body, request_content_type):
 def predict_fn(input_data, model):
     """Preprocess input data
 
-    We implement this because the default predict_fn uses .predict(), but our model is a preprocessor
-    so we want to use .transform().
+    We implement this because the default predict_fn uses .predict(), but our
+    model is a preprocessor so we want to use .transform().
 
     The output is returned in the following order:
 
@@ -161,16 +157,19 @@ def output_fn(prediction, response_content_type):
     pd.DataFrame(prediction).to_csv(strio, header=False, index=False)
     return strio.getvalue()
 
+
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("--train-test-split-ratio", type=float, default=0.3)
+    parser.add_argument("--mode", type=str, default="infer")
+    parser.add_argument("--data-dir", type=str, default="opt/ml/processing")
     args, _ = parser.parse_known_args()
 
-    print("Received arguments {}".format(args))
+    print(f"Received arguments {args}")
 
-    input_data_path = os.path.join("/opt/ml/processing/input", "census-income.csv")
+    input_data_path = os.path.join(args.data_dir, "input/census-income.csv")
 
-    print("Reading input data from {}".format(input_data_path))
+    print(f"Reading input data from {input_data_path}")
     df = pd.read_csv(input_data_path)
     df = pd.DataFrame(data=df, columns=columns)
     df.dropna(inplace=True)
@@ -179,68 +178,90 @@ if __name__ == "__main__":
 
     negative_examples, positive_examples = np.bincount(df["income"])
     print(
-        "Data after cleaning: {}, {} positive examples, {} negative examples".format(
-            df.shape, positive_examples, negative_examples
+        f"Data after cleaning: {df.shape}, {positive_examples} positive examples, "
+        f"{negative_examples} negative examples"
+    )
+
+    if args.mode == "infer":
+        model_directory = os.path.join(args.data_dir, "model")
+        print(f"Reading model from {model_directory}")
+        with tarfile.open(
+            os.path.join(model_directory, "proc_model.tar.gz"), mode="r:gz"
+        ) as archive:
+            archive.extractall(model_directory)
+        preprocess = joblib.load(os.path.join(model_directory, "model.joblib"))
+        test_features = preprocess.transform(df)
+
+        test_features_output_path = os.path.join(
+            args.data_dir, "test/test_features.csv"
         )
-    )
+        print(f"Saving test features to {test_features_output_path}")
+        pd.DataFrame(test_features).to_csv(
+            test_features_output_path, header=False, index=False
+        )
 
-    split_ratio = args.train_test_split_ratio
-    print("Splitting data into train and test sets with ratio {}".format(split_ratio))
-    X_train, X_test, y_train, y_test = train_test_split(
-        df.drop("income", axis=1), df["income"], test_size=split_ratio, random_state=0
-    )
+        test_labels_output_path = os.path.join(args.data_dir, "test/test_labels.csv")
+        print(f"Saving test labels to {test_labels_output_path}")
+        y_test = df["income"]
+        y_test.to_csv(test_labels_output_path, header=False, index=False)
+    elif args.mode == "train":
+        split_ratio = args.train_test_split_ratio
+        print(f"Splitting data into train and test sets with ratio {split_ratio}")
+        X_train, X_test, y_train, y_test = train_test_split(
+            df.drop("income", axis=1),
+            df["income"],
+            test_size=split_ratio,
+            random_state=0,
+        )
 
-    preprocess = make_column_transformer(
-        (
-            ["age", "num persons worked for employer"],
-            KBinsDiscretizer(encode="onehot-dense", n_bins=10),
-        ),
-        (
-            ["capital gains", "capital losses", "dividends from stocks"],
-            StandardScaler(),
-        ),
-        (
-            ["education", "major industry code", "class of worker"],
-            OneHotEncoder(sparse=False),
-        ),
-    )
-    print("Running preprocessing and feature engineering transformations")
-    train_features = preprocess.fit_transform(X_train)
-    test_features = preprocess.transform(X_test)
+        preprocess = make_column_transformer(
+            (
+                ["age", "num persons worked for employer"],
+                KBinsDiscretizer(encode="onehot-dense", n_bins=10),
+            ),
+            (
+                ["capital gains", "capital losses", "dividends from stocks"],
+                StandardScaler(),
+            ),
+            (
+                ["education", "major industry code", "class of worker"],
+                OneHotEncoder(sparse=False),
+            ),
+        )
+        print("Running preprocessing and feature engineering transformations")
+        train_features = preprocess.fit_transform(X_train)
+        test_features = preprocess.transform(X_test)
 
-    print("Train data shape after preprocessing: {}".format(train_features.shape))
-    print("Test data shape after preprocessing: {}".format(test_features.shape))
+        print(f"Train data shape after preprocessing: {train_features.shape}")
+        print(f"Test data shape after preprocessing: {test_features.shape}")
 
-    train_features_output_path = os.path.join(
-        "/opt/ml/processing/train", "train_features.csv"
-    )
-    train_labels_output_path = os.path.join(
-        "/opt/ml/processing/train", "train_labels.csv"
-    )
+        train_features_output_path = os.path.join(
+            args.data_dir, "train/train_features.csv"
+        )
+        train_labels_output_path = os.path.join(args.data_dir, "train/train_labels.csv")
 
-    test_features_output_path = os.path.join(
-        "/opt/ml/processing/test", "test_features.csv"
-    )
-    test_labels_output_path = os.path.join("/opt/ml/processing/test", "test_labels.csv")
+        test_features_output_path = os.path.join(
+            args.data_dir, "test/test_features.csv"
+        )
+        test_labels_output_path = os.path.join(args.data_dir, "test/test_labels.csv")
 
-    print("Saving training features to {}".format(train_features_output_path))
-    pd.DataFrame(train_features).to_csv(
-        train_features_output_path, header=False, index=False
-    )
+        print(f"Saving training features to {train_features_output_path}")
+        pd.DataFrame(train_features).to_csv(
+            train_features_output_path, header=False, index=False
+        )
 
-    print("Saving test features to {}".format(test_features_output_path))
-    pd.DataFrame(test_features).to_csv(
-        test_features_output_path, header=False, index=False
-    )
+        print(f"Saving test features to {test_features_output_path}")
+        pd.DataFrame(test_features).to_csv(
+            test_features_output_path, header=False, index=False
+        )
 
-    print("Saving training labels to {}".format(train_labels_output_path))
-    y_train.to_csv(train_labels_output_path, header=False, index=False)
+        print(f"Saving training labels to {train_labels_output_path}")
+        y_train.to_csv(train_labels_output_path, header=False, index=False)
 
-    print("Saving test labels to {}".format(test_labels_output_path))
-    y_test.to_csv(test_labels_output_path, header=False, index=False)
-    model_output_directory = "./model.joblib"
-    print("Saving model to {}".format(model_output_directory))
-    joblib.dump(preprocess, model_output_directory)
-    # tar the model
-    with tarfile.open('/opt/ml/processing/model/proc_model.tar.gz', mode='w:gz') as archive:
-        archive.add(model_output_directory, recursive=True)
+        print(f"Saving test labels to {test_labels_output_path}")
+        y_test.to_csv(test_labels_output_path, header=False, index=False)
+        joblib.dump(preprocess, "./model.joblib")
+        model_output_directory = os.path.join(args.data_dir, "model/proc_model.tar.gz")
+        print(f"Saving model to {model_output_directory}")
+        with tarfile.open(model_output_directory, mode="w:gz") as archive:
+            archive.add(model_output_directory, recursive=True)
