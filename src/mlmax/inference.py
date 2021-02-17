c3d39986fde1b7d163d6c874767d088f2b39f561
Yin Song <yinsong1986@gmail.com>
2021-02-17 16:55:38 +1100

    Modulize the inference script.

--- src/mlmax/inference.py
+++ src/mlmax/inference.py
@@ -8,23 +8,49 @@ try:
 except:
     import joblib
 
-
-if __name__ == "__main__":
-    parser = argparse.ArgumentParser()
-    parser.add_argument("--data-dir", type=str, default="opt/ml/processing")
-    args, _ = parser.parse_known_args()
-    print(f"Received arguments {args}")
-    model_path = os.path.join(args.data_dir, "model/model.tar.gz")
-    print(f"Extracting model from path: {model_path}")
+def load_model(data_dir):
+    model_path = os.path.join(data_dir, "model/model.tar.gz")
+    print(f"extracting model from path: {model_path}")
     with tarfile.open(model_path) as tar:
         tar.extractall(path=".")
-    print("Loading model")
+    print("loading model")
     model = joblib.load("model.joblib")
+    return model
 
+
+def load_test_input(data_dir):
     print("Loading test input data")
-    test_features_data = os.path.join(args.data_dir, "input/test_features.csv")
+    test_features_data = os.path.join(data_dir, "input/test_features.csv")
     X_test = pd.read_csv(test_features_data, header=None)
-    predictions = model.predict(X_test)
-    output_path = os.path.join(args.data_dir, "test/predictions.csv")
+    return X_test
+
+
+def write_data(data, data_dir, file_prefix):
+    output_path = os.path.join(data_dir, file_prefix) 
     print(f"Saving data to {output_path}")
-    pd.DataFrame(predictions).to_csv(output_path, header=False, index=False)
+    pd.DataFrame(data).to_csv(output_path, header=False, index=False)
+
+
+def parse_arg():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--data-dir",
+                        type=str,
+                        default="opt/ml/processing")
+    args, _ = parser.parse_known_args()
+    print(f"Received arguments {args}")
+    return args
+
+
+def main(args):
+    model = load_model(args.data_dir)
+    X_test = load_test_input(args.data_dir)
+    predictions = model.predict(X_test)
+    write_data(predictions, 
+               args.data_dir,
+               "test/predictions.csv")
+
+
+if __name__ == "__main__":
+    args = parse_arg()
+    main(args)
+
