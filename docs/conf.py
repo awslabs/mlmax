a737e1fc053b7e4d2f867ff1aa555eaa7ad05c87
Josiah Davis <josiah.j.davis@gmail.com>
2021-02-18 11:38:47 +0800

    Fix the table formatting issue on RTD

--- docs/conf.py
+++ docs/conf.py
@@ -66,3 +66,8 @@ html_theme = "sphinx_rtd_theme"
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ["_static"]
+
+# See here for context:
+# https://github.com/readthedocs/sphinx_rtd_theme/issues/117# See here for context:
+def setup(app):
+    app.add_stylesheet("theme_overrides.css")
