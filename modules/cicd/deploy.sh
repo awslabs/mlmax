17fcd249a4eb97ca445e1302b61ef2a9c7d7f402
Yin Song <yinsong1986@gmail.com>
2021-08-24 18:24:53 +1000

    add a deploy script for setting up the cicd.

---
+++ modules/cicd/deploy.sh
@@ -0,0 +1,27 @@
+PACKAGE_BUCKET="sagemaker-us-east-1-783128296767"
+STACK_NAME="mlmax-demo-cicd-pipeline"
+
+get_region() {
+    REGION=$(aws configure get region)
+    if [ "$REGION" == "None" ]; then
+      echo "REGION is unset in your AWS configuration";
+      exit 1;
+    else
+      echo "REGION is set to ${REGION}";
+    fi
+}
+
+deploy () {
+    aws cloudformation deploy \
+      --region=${REGION} \
+      --stack-name ${STACK_NAME} \
+      --template-file ./cicd.yaml \
+      --parameter-overrides \
+      PackageBucket=${PACKAGE_BUCKET} \
+      Region=${REGION} \
+      StackName=${STACK_NAME} \
+      --capabilities CAPABILITY_NAMED_IAM CAPABILITY_IAM CAPABILITY_AUTO_EXPAND
+}
+
+get_region
+deploy
