83fc1e1c0fa4c90e58a5caea01dc7e2495916ca9
Yin Song <yinsong1986@gmail.com>
2021-08-27 12:09:38 +1000

    update the deploy script.

--- modules/cicd/deploy.sh
+++ modules/cicd/deploy.sh
@@ -1,5 +1,12 @@
 STACK_NAME="mlmax-demo-cicd-pipeline"
 PACKAGE_BUCKET=${1:-sagemaker-us-east-1-783128296767}
+CodePipelineServiceRoleArn="arn:aws:iam::783128296767:role/mlmax-demo-cicd-pipeline-roles-CodePipelineServiceRole"
+DeployRoleStageArn="arn:aws:iam::497394617784:role/mlmax-demo-cicd-pipeline-roles-deploy-role"
+DeployRoleProdArn="arn:aws:iam::161422014849:role/mlmax-demo-cicd-pipeline-roles-deploy-role"
+InvokeStepFunctionRoleArn="arn:aws:iam::497394617784:role/mlmax-demo-cicd-pipeline-roles-InvokeStepFunctionRole"
+DevopsAccountId="783128296767"
+StageAccountId="497394617784"
+ProdAccountId="161422014849"
 
 get_region() {
     REGION=$(aws configure get region)
@@ -20,6 +27,13 @@ deploy () {
       PackageBucket=${PACKAGE_BUCKET} \
       Region=${REGION} \
       StackName=${STACK_NAME} \
+      CodePipelineServiceRoleArn=${CodePipelineServiceRoleArn} \
+      DeployRoleStageArn=${DeployRoleStageArn} \
+      DeployRoleProdArn=${DeployRoleProdArn} \
+      InvokeStepFunctionRoleArn=${InvokeStepFunctionRoleArn} \
+      DevopsAccountId=${DevopsAccountId} \
+      StageAccountId=${StageAccountId} \
+      ProdAccountId=${ProdAccountId} \
       --capabilities CAPABILITY_NAMED_IAM CAPABILITY_IAM CAPABILITY_AUTO_EXPAND
 }
 
