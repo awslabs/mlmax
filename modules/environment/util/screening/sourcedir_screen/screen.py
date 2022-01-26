b1f4436aa353e4803033190e742075a1f1b55d30
Verdi March <verdimrc@users.noreply.github.com>
2022-01-26 18:19:33 +0800

    Added screening notebook for input manifest file

--- modules/environment/util/screening/sourcedir_screen/screen.py
+++ modules/environment/util/screening/sourcedir_screen/screen.py
@@ -9,16 +9,20 @@ if __name__ == "__main__":
         type=Path,
         default=os.environ.get("SM_OUTPUT_DATA_DIR", "/opt/ml/processing/output"),
     )
-    parser.add_argument(
-        "--module", choices=["mxnet", "sklearn"], default="mxnet",
-    )
+    parser.add_argument("--module", choices=["mxnet", "sklearn", "torch"], default="mxnet")
     args = parser.parse_args()
     print(vars(args))
 
     if args.module == "mxnet":
         import mxnet as pkg
-    else:
+    elif args.module == "sklearn":
         import sklearn as pkg
+    else:
+        import torch as pkg
 
     with (args.output_data_dir / "screenings.jsonl").open("w") as f:
         f.write(f'{{"module": "{pkg.__name__}", "version": "{pkg.__version__}"}}\n')
+
+        for path in ("/opt/ml/input/data/train", "/opt/ml/processing/input"):
+            flist = list(Path(path).glob("**/*"))
+            f.write(f'{{"{path}": {flist}}}\n')
